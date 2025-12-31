28f5e3b399ccbfe62f8283816923b65371d90d25
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2025-12-31 21:59:39 +0800

    新增文件大小判断

--- src/main/java/com/github/streackmc/StreackLib/utils/HTTPServer.java
+++ src/main/java/com/github/streackmc/StreackLib/utils/HTTPServer.java
@@ -162,8 +162,12 @@ public class HTTPServer extends NanoHTTPD {
         if (!reach.getPath().startsWith(root.getCanonicalPath())) {
           return newFixedLengthResponse(Response.Status.FORBIDDEN, NanoHTTPD.MIME_PLAINTEXT, "403 Forbidden");
         }
-        if (reach.exists() && reach.isFile()) {
-          // 判断文件是否合法
+        if (reach.exists() && reach.isFile()) {// 判断文件是否合法
+          // 判断大小是否合法
+          if (reach.length() > MAX_FILE_SIZE) {
+            return newFixedLengthResponse(Response.Status.BAD_REQUEST, NanoHTTPD.MIME_PLAINTEXT, "413 Payload Too Large");
+          }
+          // 返回文件
           return newFixedLengthResponse(Response.Status.OK, "application/octet-stream", new FileInputStream(reach), reach.length());
         } else {
           // 文件不存在

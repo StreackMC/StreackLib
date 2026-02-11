97d9f57c91c55bf8844d0a260a0cd6b55b975590
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-11 13:16:02 +0800

    修复时间戳超范围问题

--- src/main/java/com/github/streackmc/StreackLib/StreackLib.java
+++ src/main/java/com/github/streackmc/StreackLib/StreackLib.java
@@ -1,6 +1,7 @@
 package com.github.streackmc.StreackLib;
 
 import java.io.File;
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.util.ArrayDeque;
@@ -113,8 +114,10 @@ public final class StreackLib {
     long t = (time == null)
         ? System.currentTimeMillis()
         : time.longValue();
-    if (t < Instant.MIN.getEpochSecond() * 1000 || t > Instant.MAX.getEpochSecond() * 1000) {
-      throw new IllegalArgumentException("时间戳超出有效范围：");
+    try {
+        Instant.ofEpochMilli(t);// 超范围会抛异常
+    } catch (DateTimeException e) {
+        throw new IllegalArgumentException("时间戳超出有效范围：" + t, e);
     }
     String f = (format == null || format.isEmpty())
         ? "yyyy-MM-dd HH:mm:ss.SSSS"

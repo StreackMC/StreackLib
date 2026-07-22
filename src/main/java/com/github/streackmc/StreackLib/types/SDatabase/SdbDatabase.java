bb782e5cc6ac3c43bd2287c81b7e9edaf69132c3
kdxiaoyi [MacMini] <kdxiaoyi@outlook.com>
2026-07-22 11:47:35 +0800

    合并散落的Backend

--- src/main/java/com/github/streackmc/StreackLib/types/SDatabase/SdbDatabase.java
+++ src/main/java/com/github/streackmc/StreackLib/types/SDatabase/SdbDatabase.java
@@ -1,5 +1,6 @@
 package com.github.streackmc.StreackLib.types.SDatabase;
 
+import java.sql.Connection;
 import java.util.function.Consumer;
 
 import javax.annotation.Nonnull;
@@ -10,6 +11,8 @@ import com.github.streackmc.StreackLib.errors.ConfigNotFoundException;
 import com.github.streackmc.StreackLib.errors.InvaildConfigException;
 import com.github.streackmc.StreackLib.types.SConfig;
 import com.github.streackmc.StreackLib.types.StreackLibNewable;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * <h2>SdbDatabase</h2>
@@ -162,5 +165,72 @@ public class SdbDatabase extends StreackLibNewable {
   public SdbDatabase moveTable(@Nonnull String name) {
     // TODO: implement
     return this;
+  }
+}
+
+/**
+ * <h2>Backend</h2>
+ * 数据库后端接口，封装连接池/single-connection的借还操作。
+ *
+ * @since 0.6.0
+ */
+interface Backend {
+
+  /**
+   * 从池中借出一个连接。
+   *
+   * @return 一个可用的{@link Connection}。调用者的{@code close()}行为：
+   *         <ul>
+   *         <li><b>SqliteBackend</b>：被拦截，不真正关闭</li>
+   *         <li><b>MysqlBackend</b>：归还给 HikariCP 池</li>
+   *         </ul>
+   */
+  Connection borrowConnection() throws Exception;
+
+  /** 销毁整个连接池/关闭底层连接 */
+  void close();
+}
+
+/**
+ * <h2>MysqlBackend</h2>
+ *
+ * MySQL HikariCP 连接池后端。构造时创建 DataSource，
+ * {@code borrowConnection()} 返回池中空闲连接，调用者的
+ * {@code close()} 自动归还连接给池。
+ *
+ * @since 0.6.0
+ */
+class MysqlBackend implements Backend {
+
+  private final HikariDataSource ds;
+
+  MysqlBackend(String host, int port, String database, String user, String password) {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + database);
+    config.setUsername(user);
+    config.setPassword(password);
+
+    config.setMaximumPoolSize(10);
+    config.setMinimumIdle(2);
+    config.setConnectionTimeout(30000);
+    config.setIdleTimeout(600000);
+    config.setMaxLifetime(1800000);
+
+    config.addDataSourceProperty("cachePrepStmts", "true");
+    config.addDataSourceProperty("prepStmtCacheSize", "250");
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+    config.addDataSourceProperty("useServerPrepStmts", "true");
+
+    this.ds = new HikariDataSource(config);
+  }
+
+  @Override
+  public Connection borrowConnection() throws Exception {
+    return ds.getConnection();
+  }
+
+  @Override
+  public void close() {
+    ds.close();
   }
 }
